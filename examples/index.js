cda39814651087ec3dc021da18fd0ea3c0bdb197
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-04-28 00:13:30 +0200

    Add iframe to example

--- examples/index.js
+++ examples/index.js
@@ -9,6 +9,11 @@ let markdown = `
 
 ![logo_ironhack_blue 7](https://user-images.githubusercontent.com/23629340/40541063-a07a0a8a-601a-11e8-91b5-2f13e4e6b441.png)
 
+<iframe height="265" style="width: 100%;" scrolling="no" title="&quot;Cropping&quot; an Image With CSS" src="//codepen.io/nizaroni/embed/pKmBag/?height=265&theme-id=light&default-tab=result" frameborder="no" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href='https://codepen.io/nizaroni/pen/pKmBag/'>&quot;Cropping&quot; an Image With CSS</a> by Nizar Khalife
+  (<a href='https://codepen.io/nizaroni'>@nizaroni</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
+
 ---------------------------------------------------
 
 Boom
